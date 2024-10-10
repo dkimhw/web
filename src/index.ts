ae1955919e6fcce2cf351603688777c36322dbc3
David Kim <dkhw90@gmail.com>
2024-10-10 20:39:13 +0200

    Create user class with get, set, on, and trigger methods

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
+import { User } from "./models/User";
 
-console.log("Hi there")
+
+const user = new User({ name: 'asdf', age: 20 });
+user.on('click', () => {
+  console.log('clicked');
+});
+
+user.on('click', () => {
+  console.log('another clicked');
+});
+
+user.on('change', () => {
+  console.log('changed');
+});
+
+user.trigger('click');
